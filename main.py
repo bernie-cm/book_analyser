7dc5051f05e1abae991edee04c9ac46b8ffe6eac
bernie-cm <bernie.camejo@icloud.com>
2024-08-19 19:12:14 +1000

    add word counter function

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ def main():
     # function to open the file and get the contents
     book_path = "books/frankenstein.txt"
     book_text = get_book_contents(book_path)
-    # print the text
-    print(book_text)
+    # count all the words in book_text
+    total_words = count_words(book_text)
+    print(total_words)
 
 def get_book_contents(path):
     with open(path) as fin:
         return fin.read()
+    
+def count_words(text):
+    list_of_words = text.split()
+    return len(list_of_words)
     
 
 if __name__ == "__main__":
